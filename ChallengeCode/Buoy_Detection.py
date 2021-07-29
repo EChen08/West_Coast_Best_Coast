73da6bd06245924b4deffc15c9a2b629ac86c319
Shaunak Roy <me.shaunak.roy@gmail.com>
2021-07-29 10:25:04 -0700

    bleh

--- ChallengeCode/Buoy_Detection.py
+++ ChallengeCode/Buoy_Detection.py
@@ -87,7 +87,6 @@ def green_update(im): #updates green buoy data
     green_buoys = 0
     
     img = cv2.resize(im, (640, 480)) # resize each image
-    #img = np.flip(img, axis=0) # flip the image
 
     imhsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # convert to HSV
     median_blur = cv2.medianBlur(imhsv, 9) # apply median blur filter to smoothen the HSV image, denoise
@@ -136,7 +135,6 @@ def red_update(im): # updates red buoy data
     red_buoys = 0
     
     img = cv2.resize(im, (640, 480)) # resize each image
-    #img = np.flip(img, axis=0) # flip the image
 
     imhsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # convert to HSV
     median_blur = cv2.medianBlur(imhsv, 5) # apply median to smoothen the HSV image, denoise
@@ -177,7 +175,7 @@ def red_update(im): # updates red buoy data
     return red_buoys
 
 def run(image):
-    im = cv2.imread(r'.\frames\frame_1627338749.jpg')
+    im = cv2.imread(image)
     red_buoys = red_update(im)
     green_buoys = green_update(im)
     print(red_pixel_means, green_pixel_means)
